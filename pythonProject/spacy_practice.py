412c275b45b8fe308dbe3b88a91cb7cd2e2aa0f8
gylertaydos <tylergaydos@outlook.com>
2024-02-02 11:16:54 -0500

    python spacy practice

--- pythonProject/spacy_practice.py
+++ pythonProject/spacy_practice.py
@@ -5,9 +5,15 @@ nlp = spacy.load('en_core_web_sm')
 avatarSpeeches = open('avatarSpeeches.txt', 'r', encoding='utf8')
 words = avatarSpeeches.read()
 wordstrings = str(words)
-# print(wordstrings)
+print(wordstrings)
+
+# dracula = open('grimm.txt', 'r', encoding='utf8')
+# avatarSpeeches= nlp
+# dracula= nlp
+# print(avatarSpeeches, '<->', dracula, avatarSpeeches.similarity(dracula))
+# tried to get it to compare the two documents like on the web guide but it couldn't
 
 avatarWords = nlp(wordstrings)
 for token in avatarWords:
     # if token.pos_ == "VERB":
-    print(token.text, "---->", token.pos_, ":::::", token.lemma_)
+    print(token.text, "---->", token.pos_, ":::::", token.lemma_, token.dep_, token.is_stop)
